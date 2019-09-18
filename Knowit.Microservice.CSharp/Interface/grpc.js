15deebeddefd04f4aa6d31ddb065dad92f2fe1bc
Gøran Humlestøl <goran.humlestol@knowit.no>
2019-09-18 10:08:09 +0200

    Update protoc config to work with multiple proto files that depend on each other

--- Knowit.Microservice.CSharp/Interface/grpc.js
+++ Knowit.Microservice.CSharp/Interface/grpc.js
@@ -1,11 +1,14 @@
 const child_process = require("child_process");
 const glob = require("glob");
+const fs = require("fs");
+
+fs.mkdirSync("Contracts");
 
 const files = glob.sync("../Contracts/*.proto");
 child_process.execSync([
   "protoc",
   ...files,
-  "--proto_path=..",
-  "--js_out=import_style=commonjs:.",
-  "--grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:."
+  "--proto_path=../Contracts",
+  "--js_out=import_style=commonjs:Contracts",
+  "--grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:Contracts"
 ].join(" "));
